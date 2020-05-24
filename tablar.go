a4fa35fffa55eaa54087f3ad39f8099c80ca7e55
maurya373 <maurya373@gmail.com>
2020-05-24 17:11:14 -0500

    generate playable notes on guitar for a scale

--- tablar.go
+++ tablar.go
@@ -4,15 +4,50 @@ import "fmt"
 
 func main() {
 
-	notes := [12]string{"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"}
+	notes := []string{"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"}
 	key := "G"
 	mode := "Phrygian"
 
-	fmt.Println(getScale(notes, key, mode))
+	var scale = getScale(notes, key, mode)
+	fmt.Println(scale)
+
+	var fretBoard = createFretBoard(notes)
+	var playableNotes = getPlayableNotes(fretBoard, scale)
+	fmt.Println(playableNotes)
+}
+
+// Generate a Standard E Tuning fretboard with notes for open strings up to fret 11
+func createFretBoard(notes []string) [][]string {
+
+	var fretBoard [][]string
+	fretBoard = append(fretBoard, wrapNotesForScale(notes, "E"))
+	fretBoard = append(fretBoard, wrapNotesForScale(notes, "A"))
+	fretBoard = append(fretBoard, wrapNotesForScale(notes, "D"))
+	fretBoard = append(fretBoard, wrapNotesForScale(notes, "G"))
+	fretBoard = append(fretBoard, wrapNotesForScale(notes, "B"))
+	fretBoard = append(fretBoard, wrapNotesForScale(notes, "E"))
+	return fretBoard
+}
+
+// Generate a list of frets that are valid to play given the scale
+func getPlayableNotes(fretBoard [][]string, scale []string) [][]int {
+
+	var playableNotes [][]int
+	for i := 0; i < len(fretBoard); i++ {
+		var stringNotes []int
+		for j := 0; j < len(fretBoard[i]); j++ {
+			// If note is in scale, add to playable notes on string
+			if getIndexForElement(scale, fretBoard[i][j]) != -1 {
+				stringNotes = append(stringNotes, j)
+			}
+		}
+		playableNotes = append(playableNotes, stringNotes)
+	}
+	return playableNotes
 }
 
 // Get the list of notes, in order, that form the scale for the key and the mode
-func getScale(notes [12]string, key string, mode string) []string {
+func getScale(notes []string, key string, mode string) []string {
 
 	var wrappedNotesForKey = wrapNotesForScale(notes, key)
 	var modeFormula = getModeFormula(mode)
@@ -25,7 +60,7 @@ func getScale(notes [12]string, key string, mode string) []string {
 }
 
 // Return the list of all notes rotated to start with the key
-func wrapNotesForScale(notes [12]string, key string) []string {
+func wrapNotesForScale(notes []string, key string) []string {
 
 	var noteList []string
 	index := getIndexForElement(notes, key)
@@ -40,7 +75,7 @@ func wrapNotesForScale(notes [12]string, key string) []string {
 }
 
 // Get the index where the key is located in the notes list
-func getIndexForElement(notes [12]string, key string) int {
+func getIndexForElement(notes []string, key string) int {
 
 	for i := range notes {
 		if notes[i] == key {
@@ -54,13 +89,13 @@ func getIndexForElement(notes [12]string, key string) int {
 func getModeFormula(mode string) []int {
 
 	switch mode {
-	case "Major", "Ionian": 	return []int{0, 2, 4, 5, 7, 9, 11}
-	case "Dorain": 			return []int{0, 2, 3, 5, 7, 9, 10}
-	case "Phrygian":		return []int{0, 1, 3, 5, 7, 8, 10}
-	case "Lydian": 			return []int{0, 2, 4, 6, 7, 9, 11}
-	case "Mixolydian": 		return []int{0, 2, 4, 5, 7, 9, 10}
-	case "Minor", "Aeolian": 	return []int{0, 2, 3, 5, 7, 8, 10}
-	case "Locrian": 		return []int{0, 1, 3, 5, 6, 8, 10}
-	default: 			return []int{}
+	case "Major", "Ionian": return []int{0, 2, 4, 5, 7, 9, 11}
+	case "Dorain": return []int{0, 2, 3, 5, 7, 9, 10}
+	case "Phrygian": return []int{0, 1, 3, 5, 7, 8, 10}
+	case "Lydian": return []int{0, 2, 4, 6, 7, 9, 11}
+	case "Mixolydian": return []int{0, 2, 4, 5, 7, 9, 10}
+	case "Minor", "Aeolian": return []int{0, 2, 3, 5, 7, 8, 10}
+	case "Locrian": return []int{0, 1, 3, 5, 6, 8, 10}
+	default: return []int{}
 	}
 }
