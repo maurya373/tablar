f6c7f6aafb69daffe9e784860e825ad4959e8d6e
maurya373 <maurya373@gmail.com>
2020-05-24 18:20:42 -0500

    add riff generation

--- tablar.go
+++ tablar.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+import "math/rand"
+import "time"
 
 func main() {
 
@@ -9,14 +14,48 @@ func main() {
 	mode := "Phrygian"
 
 	var scale = getScale(notes, key, mode)
-	fmt.Println(scale)
+	fmt.Println(key,mode,":",scale)
 
 	var fretBoard = createFretBoard(notes)
 	var playableNotes = getPlayableNotes(fretBoard, scale)
-	fmt.Println(playableNotes)
+
+	var riff = generateRiff(playableNotes, 8)
+	printRiff(riff)
+}
+
+// Generate a random riff with the number of playable notes defined
+// TODO: Provide constraints on how far away the notes can be from one another
+func generateRiff(playableNotes [][]int, count int) [][]string {
+
+	var riff [][]string
+	for i := 0; i < count; i++ {
+		var stringNum = random(5)
+		var noteInScale = random(6)
+		var fret = playableNotes[stringNum][noteInScale]
+
+		var note = []string{"-","-","-","-","-","-"}
+		note[stringNum] = strconv.Itoa(fret)
+		riff = append(riff, note)
+	}
+	return riff
+}
+
+// Print the generated tablature
+func printRiff(tab [][]string) {
+
+	for i := len(tab[0])-1; i >= 0; i-- {
+		for j := 0; j < len(tab); j++ {
+			if j > len(tab) || i > len(tab[0]) {
+				continue
+			}
+			fmt.Print(tab[j][i],"-")
+		}
+		fmt.Println()
+	}
 }
 
 // Generate a Standard E Tuning fretboard with notes for open strings up to fret 11
+// TODO: Support drop tunings
 func createFretBoard(notes []string) [][]string {
 
 	var fretBoard [][]string
@@ -98,4 +137,11 @@ func getModeFormula(mode string) []int {
 	case "Locrian": return []int{0, 1, 3, 5, 6, 8, 10}
 	default: return []int{}
 	}
+}
+
+// Return a random number between 0 and the passed-in limit
+func random(limit int) int {
+	rand.Seed(time.Now().UnixNano())
+	min := 0
+	return rand.Intn(limit - min + 1) + min
 }
